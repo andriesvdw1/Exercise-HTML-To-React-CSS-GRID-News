edbc9f1009b3ad755e01bdab423e88ad05c39a10
andriesvdw1 <andriesvdw1@gmail.com>
2025-04-02 16:07:30 +0200

    Add header component

---
+++ exercise-news/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '.Header.css'
+function Header() {
+  return (
+    <header>
+    <h1>Latest News</h1>
+    <h2>Technology</h2>
+    </header>
+  )
+}
+
+export default Header
